13a912316a9d4b9f87507a11ba8124bcf8d7f07a
Adam Grace <S00188523@atu.ie>
2023-11-26 11:49:20 +0000

    cors added to support angular project

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require('express')
-const app = express()
+const https = require('https')
+const cors = require('cors')
+
 
 require('dotenv').config();
 
+const app = express()
 app.use(express.json());
 
-require('./database')
+const db = require('./database')
 const port = process.env.PORT || 3001
-
-app.use(express.json());
 
 const {Team} = require('./models/teams')
 const teams = require('./routes/teams')
 
+app.use(express.json());
+
+app.use(cors(corsOptions))
+
 app.use('/api/v1/teams', teams)
+
+app.use('/teams', cors(), teams);
+
+var corsOptions = {
+    origin: 'http://loclahost:4200',
+    optionsSuccessStatus: 200
+  }
 
 app.listen(port, () => console.log(`Assignment 1 app listening on port ${port}!`))
